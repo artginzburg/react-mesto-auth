7e2c366fa24495305c5a18a3c5b6e688d44f7361
artginzburg <art.ginzburg@ya.ru>
2021-07-11 16:42:15 +0300

    use more imports and env variables

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,11 @@ import { memo } from 'react';
 
 import logo from '../images/logo.svg';
 
+import { homepage } from '../../package.json';
+
 const Header = memo(() => (
   <header className="header">
-    <a
-      target="_self"
-      href="https://github.com/artginzburg/react-mesto-auth"
-      className="logo header__logo"
-    >
+    <a target="_self" href={homepage} className="logo header__logo">
       <img className="logo__img" alt="Mesto" src={logo} />
     </a>
   </header>
