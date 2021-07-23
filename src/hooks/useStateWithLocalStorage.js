6778154480e30b40b178f89e46a688e60dc039a2
artginzburg <art.ginzburg@ya.ru>
2021-07-23 15:52:16 +0300

    better error handling for useStateWithLocalStorage

--- src/hooks/useStateWithLocalStorage.js
+++ src/hooks/useStateWithLocalStorage.js
@@ -2,28 +2,34 @@ import { useState, useCallback } from 'react';
 
 import useEventListener from './useEventListener';
 
-export default function useStateWithLocalStorage(computed, defaultForComputed) {
-  const [currentState, updateCurrentState] = useState(
-    localStorage[computed] && localStorage[computed] !== 'undefined'
-      ? JSON.parse(localStorage[computed])
-      : defaultForComputed
-  );
+export default function useStateWithLocalStorage(key, initialValue) {
+  const [currentState, updateCurrentState] = useState(() => {
+    try {
+      const item = localStorage[key];
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
 
   const setCurrentState = useCallback(
     (value) => {
-      localStorage[computed] = JSON.stringify(value);
+      // const valueToStore = value instanceof Function ? value(currentState) : value;
+
+      localStorage[key] = JSON.stringify(value);
       updateCurrentState(value);
     },
-    [computed]
+    [key]
   );
 
   const localStorageChanged = useCallback(
     (e) => {
-      if (e.key === computed) {
-        updateCurrentState(e.newValue ? JSON.parse(e.newValue) : defaultForComputed);
+      if (e.key === key) {
+        updateCurrentState(e.newValue ? JSON.parse(e.newValue) : initialValue);
       }
     },
-    [computed, defaultForComputed]
+    [key, initialValue]
   );
 
   useEventListener('storage', localStorageChanged);
